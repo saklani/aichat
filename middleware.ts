7ec4b07292ec8950fadde9ec6bbbcaad1f625162
saklani <50768838+saklani@users.noreply.github.com>
2025-02-17 05:29:35 -0500

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,16 @@
 import { getSessionCookie } from "better-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-
-function handleRedirection(req: NextRequest,
-    redirectTo: string,
-    condition: boolean) {
-    if (condition) {
-        return NextResponse.redirect(new URL(redirectTo, req.nextUrl.origin));
-    }
-    return NextResponse.next()
-}
-
  
 export async function middleware(request: NextRequest) {
 	const sessionCookie = getSessionCookie(request);
-    const loggedIn = !!sessionCookie
-
-    if (request.nextUrl.pathname.startsWith("/chat")
+ 
+    if (!sessionCookie && request.nextUrl.pathname.startsWith("/chat")
         || request.nextUrl.pathname.startsWith("/settings")
     ) {
-        return handleRedirection(request, "/login", !loggedIn);
+        return NextResponse.redirect(new URL("/login", request.nextUrl.origin));
     }
-    if (request.nextUrl.pathname.startsWith("/login")
-        || request.nextUrl.pathname.startsWith("/register")
-    ) {
-        return handleRedirection(request, "/chat", loggedIn);
-    }
+
     return NextResponse.next();
 }
  
