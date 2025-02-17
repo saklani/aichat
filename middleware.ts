1665b04107a67ddd7436272e5a563c21fc0cb9fb
saklani <50768838+saklani@users.noreply.github.com>
2025-02-17 05:34:20 -0500

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,16 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
 	const sessionCookie = getSessionCookie(request);
  
-    if (!sessionCookie && request.nextUrl.pathname.startsWith("/chat")
-        || request.nextUrl.pathname.startsWith("/settings")
+    if (!sessionCookie && (request.nextUrl.pathname.startsWith("/chat")
+        || request.nextUrl.pathname.startsWith("/settings"))
     ) {
         return NextResponse.redirect(new URL("/login", request.nextUrl.origin));
+    }
+
+    if (sessionCookie && (request.nextUrl.pathname.startsWith("/login")
+        || request.nextUrl.pathname.startsWith("/register"))
+    ) {
+        return NextResponse.redirect(new URL("/", request.nextUrl.origin));
     }
 
     return NextResponse.next();
