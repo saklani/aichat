d9f7ac4450f5436593c626412e2be774e7700f8a
saklani <50768838+saklani@users.noreply.github.com>
2025-02-17 05:14:25 -0500

    fix

--- lib/server/db/queries/user.ts
+++ lib/server/db/queries/user.ts
@@ -3,11 +3,6 @@ import { schema } from '..';
 import { db } from "../db";
 import { execute } from "./utils";
 
-
-export async function createUser(data: Pick<schema.User, "googleId" | "email" | "passwordHash">) {
-    return execute('insert user', () => db.insert(schema.users).values({ ...data }).returning().then(res => res.at(0)))
-}
-
 export async function updateUser({ id, ...rest }: schema.User) {
     return execute(`update user ${id}`, () => db.update(schema.users).set(rest).where(eq(schema.users.id, id)).returning().then(res => res.at(0)))
 }
@@ -21,9 +16,6 @@ export async function getUser({ id }: Pick<schema.User, "id">) {
     return execute(`get user ${id}`, () => db.query.users.findFirst({ where: eq(schema.users.id, id), columns: { id: true, email: true } }))
 }
 
-export async function getUserByGoogleId({ googleId }: Pick<schema.User, "googleId">) {
-    return execute(`get user ${googleId}`, () => db.query.users.findFirst({ where: eq(schema.users.googleId, googleId!), columns: { id: true, email: true } }))
-}
 
 export async function getUserByEmail({ email }: Pick<schema.User, "email">) {
     return execute(`get user by email ${email}`, () => db.query.users.findFirst({ where: eq(schema.users.email, email) }))
