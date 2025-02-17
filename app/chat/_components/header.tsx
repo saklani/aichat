38e7e128a7f9f56855381fc5830e8c466b61bb03
saklani <50768838+saklani@users.noreply.github.com>
2025-02-17 06:16:16 -0500

    move useRouter to redirect

--- app/chat/_components/header.tsx
+++ app/chat/_components/header.tsx
@@ -1,20 +1,21 @@
-"use client"
-
 import { SidebarHeader, SidebarGroup, SidebarGroupLabel, SidebarGroupAction } from "@/components/ui/sidebar";
-import { Plus  } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import { redirect } from "next/navigation";
 
-export function Header() {
-
-    const router = useRouter()
+export async function Header() {
     return (
         <SidebarHeader>
             <SidebarGroup>
                 <SidebarGroupLabel>Sable</SidebarGroupLabel>
-                <SidebarGroupAction title="New Chat" onClick={() => router.push("/chat")}>
-                    <Plus strokeWidth={0.9} /> <span className="sr-only">New Chat</span>
-                </SidebarGroupAction>
+                <form action={async () => {
+                    "use server"
+                    redirect("/chat")
+                }}>
+                    <SidebarGroupAction title="New Chat" type="submit">
+                        <Plus strokeWidth={0.9} /> <span className="sr-only">New Chat</span>
+                    </SidebarGroupAction>
+                </form>
             </SidebarGroup>
-        </SidebarHeader>
+        </SidebarHeader >
     )
 }
