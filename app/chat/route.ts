0d4efb67e24576d893b6964d8ff886011c9af746
saklani <50768838+saklani@users.noreply.github.com>
2025-02-18 00:15:51 -0500

    move redirect to route side

--- app/chat/route.ts
+++ app/chat/route.ts
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { randomUUID } from "node:crypto";
 
-export default function Page() {
+export function GET() {
     const id = randomUUID();
     
     redirect(`/chat/${id}?`);
