5868c9a4eea4c7d64e47088085c215f8d8b4eb0b
saklani <50768838+saklani@users.noreply.github.com>
2025-02-27 13:09:30 -0500

    fix logo

--- components/landing-page/example-image.tsx
+++ components/landing-page/example-image.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export function ExampleImage() {
     return (
-        <div className=" border">
-            <Image src="https://sable-assets-public.s3.us-east-1.amazonaws.com/example.png" alt="Sample" width={0} height={0} sizes="100vw"  className="w-full h-full object-cover" priority/>
+        <div className="border w-full">
+            <Image src="https://sable-assets-public.s3.us-east-1.amazonaws.com/example.png" alt="Sample" width={0} height={0} sizes="w-full"  className="w-full h-full object-cover" priority/>
         </div>
     )
 }
